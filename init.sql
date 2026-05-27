36273d61f95610867a0d8eb2aa36e760e3172589
Varad Sawant <varadsawant1996@gmail.com>
2026-05-27 14:01:37 +0530

    dockerize project

---
+++ init.sql
@@ -0,0 +1,8 @@
+-- init.sql
+CREATE TABLE IF NOT EXISTS users (
+    user_id SERIAL PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    phone_number VARCHAR(20)
+);
