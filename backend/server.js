ca42f5c6bff9cccec05049fe2169681848454f69
Varad Sawant <varadsawant1996@gmail.com>
2026-06-01 14:52:38 +0530

    new commit

--- backend/server.js
+++ backend/server.js
@@ -84,6 +84,6 @@ app.delete('/api/users/:id', async (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+app.listen(PORT,'0.0.0.0', () => {
     console.log(`Server running on port ${PORT}`);
 });
